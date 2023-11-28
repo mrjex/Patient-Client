758aa9c082f42e79c5d345c6be704b4e71c80a20
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:49:10 +0100

    #3 (ongoing) Add AboutUs veiw

--- client/src/views/AboutUs.vue
+++ client/src/views/AboutUs.vue
@@ -1,5 +1,6 @@
 <template>
   <!--Wireframe - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#about-us -->
+  <div>About us</div>
 </template>
 
 <script>
