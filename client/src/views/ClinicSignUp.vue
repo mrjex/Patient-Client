26bc8b4c50bdb40b68708c0170a57255f6e03489
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:50:48 +0100

    #3 (ongoing) Add clinic how to sign up veiw

--- client/src/views/ClinicSignUp.vue
+++ client/src/views/ClinicSignUp.vue
@@ -1,5 +1,6 @@
 <template>
 <!-- Wireframe - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#clinic-sign-up -->
+  <div>Clinic sign up</div>
 </template>
 
 <script>
