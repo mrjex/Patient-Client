114d6f00f515ad8a0c605f24c943e41986d9bc48
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:49:44 +0100

    #3 (ongoing) Add Authentication veiw

--- client/src/views/Authentication.vue
+++ client/src/views/Authentication.vue
@@ -4,6 +4,7 @@ Wireframes:
  Sign uo - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#sign-up
  Sign in - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#sign-in
 -->
+  <p>Authentication</p>
 </template>
 
 <script>
