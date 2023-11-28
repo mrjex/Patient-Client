6223d0d5bcd2a52cfab7d43dcea601540d070d7b
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:56:48 +0100

    #3 (ongoing) Add header componant

---
+++ client/src/component/DentaNoidHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary" id="main-header" style="font-size: 1.3rem;">
+      <b-navbar-brand href="/">
+        DentaNoid
+<!--TODO: Insert logo        <img src="">-->
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/clinics">Clinics</b-nav-item>
+          <b-nav-item href="/aboutus">About us</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" href="/mybookings">My bookings</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" href="/notifications">Notifications</b-nav-item>
+
+        </b-navbar-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="!isLoggedIn" @click="signInSignOut">Login</b-nav-item>
+          <b-nav-item v-if="!isLoggedIn" @click="signInSignOut">Sign Up</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" @click="signInSignOut">Sign out</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header.vue',
+  data() {
+    return {
+      isLoggedIn: false
+    }
+  },
+  methods: {
+    signInSignOut() {
+      if (!this.isLoggedIn) {
+        localStorage.setItem('loggedIn', true)
+        this.isLoggedIn = true
+      } else {
+        localStorage.setItem('loggedIn', false)
+        this.isLoggedIn = false
+      }
+    }
+  },
+  mounted() {
+    this.isLoggedIn = localStorage.getItem('loggedIn')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
