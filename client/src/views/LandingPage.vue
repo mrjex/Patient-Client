99b0561bbcbbd6ebc79811defd937c30ec768566
JamesKlouda <gusklouja@student.gu.se>
2023-11-28 01:54:03 +0100

    #3 (ongoing) Add Landing page veiw

--- client/src/views/LandingPage.vue
+++ client/src/views/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <!--Wireframe - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#landing-page  -->
+  <p>Landing page</p>
 </template>
 
 <script>
