b0f96fb8f80d49830f5da22b9256a52099e9b9a3
JamesKlouda <gusklouja@student.gu.se>
2023-11-30 15:12:32 +0100

    #6 (ongoing) add Landing page
    This commit adds a landing page that resembles the wireframe. The page is responsive yet this will be implemented in later commits

--- client/src/views/LandingPage.vue
+++ client/src/views/LandingPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!--Wireframe - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#landing-page  -->
-  <div style="border-color: yellow">
+  <body>
     <div class="container">
       <div class="carousel">
         <b-carousel
@@ -10,51 +10,26 @@
           controls
           indicators
           background="#ababab"
-          img-width="1024"
-          img-height="480"
           style="text-shadow: 1px 1px 2px #333;"
           @sliding-start="onSlideStart"
           @sliding-end="onSlideEnd"
         >
-
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"> <!--Select dental img -->
             <h1>Need to consult a specialist?</h1>
             <b-btn pill variant="success">Book</b-btn>
           </b-carousel-slide>
 
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Need to consult a specialist?</h1>
-            <b-btn pill variant="success">Book</b-btn>
+          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"><!--Select dental img -->
+            <h1>Find clinics near you!</h1>
+            <b-btn pill variant="success">Clinics</b-btn>
           </b-carousel-slide>
 
-          <!-- Slides with custom text -->
-          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Need to consult a specialist?</h1>
-            <b-btn pill variant="success">Book</b-btn>
+          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"><!--Select dental img -->
+            <h1>Who are we?</h1>
+            <b-btn pill variant="success">About us</b-btn>
           </b-carousel-slide>
 
-          <!-- Slides with img slot -->
-          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-          <b-carousel-slide>
-            <template #img>
-              <img
-                class="d-block img-fluid w-100"
-                width="1024"
-                height="480"
-                src="https://picsum.photos/1024/480/?image=55"
-                alt="image slot"
-              >
-            </template>
-            <h1>Need to consult a specialist?</h1>
-          </b-carousel-slide>
         </b-carousel>
-
-        <!--        <p class="mt-4">-->
-        <!--          Slide #: {{ slide }}<br>-->
-        <!--          Sliding: {{ sliding }}-->
-        <!--        </p>-->
       </div>
     </div>
     <div class="container">
@@ -62,20 +37,19 @@
         <div class="col-2"></div>
         <div class="col-8">
           <h1>What do we offer?</h1>
-          <p>
-            At dentanoid we offer a seamless online booking experience for multiple dental clincics across Gothenburg.
-            Fobar bar fo lorem ipsum ipsum ipsumusus loren.
-            Fo fo fo bar bar bar lorem ipsumus sumen. Fobaricus fornum forus bar ipsum lorem fo barn ipsi pipsi.
+          <p><!--Rewrite Text with group -->
+            At Dentanoid, we provide a seamless online booking experience for multiple dental clinics across Gothenburg.
+            Our platform ensures a user-friendly interface,
+            making it easy for you to schedule appointments with various dental practitioners in the area.
+            we're all about real and meaningful communication.
+            Whether you're looking for routine check-ups, cosmetic dentistry, or specialized treatments, Dentanoid has you covered.
+            Our commitment is to offer a hassle-free booking process, allowing you to prioritize your oral health with convenience and efficiency.
           </p>
         </div>
         <div class="col-2">
-          <div class='user-rev'>
-            <h3>Lena</h3>
-            <p>'Fobar bar fo lorem ipsum ipsum'</p>
-          </div>
-          <div class='user-rev'>
-            <h3>Adam</h3>
-            <p>'Fobar bar fo lorem ipsum ipsum'</p>
+          <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'"/>
+          <div>
+            <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'"/>
           </div>
         </div>
         <div class="col-2"></div>
@@ -89,19 +63,22 @@
               <b-btn size="lg" pill variant="success">Sign in</b-btn>
             </div>
             <div class="col-4">
-              <b-btn size="lg" pill variant="success">Book</b-btn>
+              <b-btn size="lg" pill variant="success">Book </b-btn>
             </div>
           </div>
         </div>
         <div class="col-2"></div>
       </div>
     </div>
-  </div>
+  </body>
 </template>
 
 <script>
+import QuoteCard from '@/component/QuoteCard.vue'
+
 export default {
   name: 'LandingPage.vue',
+  components: { QuoteCard },
   data() {
     return {
       slide: 0,
@@ -121,15 +98,6 @@ export default {
 
 <style scoped>
 .carousel {
-  border-color: #000000;
-  border-style: solid;
-  border-width: 1px;
-  margin-bottom: 10px;
-}
-
-.user-rev {
-  border-style: solid;
-  border-color: black;
   margin-bottom: 10px;
 }
 
