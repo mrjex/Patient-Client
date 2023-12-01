6eb815e96a3c698c06dba5269b57b4aeda7a6d3f
JamesKlouda <gusklouja@student.gu.se>
2023-12-01 17:35:18 +0100

    #6 (complete) add responsivness
    This commit adds rwd and links the buttons to the correct pages.

--- client/src/views/LandingPage.vue
+++ client/src/views/LandingPage.vue
@@ -1,75 +1,85 @@
 <template>
   <!--Wireframe - https://git.chalmers.se/courses/dit355/2023/student-teams/dit356-2023-20/group-20-distributed-systems/-/wikis/Patient-gui#landing-page  -->
   <body>
-    <div class="container">
-      <div class="carousel">
-        <b-carousel
-          id="carousel-1"
-          v-model="slide"
-          :interval="4000"
-          controls
-          indicators
-          background="#ababab"
-          style="text-shadow: 1px 1px 2px #333;"
-          @sliding-start="onSlideStart"
-          @sliding-end="onSlideEnd"
-        >
-          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"> <!--Select dental img -->
-            <h1>Need to consult a specialist?</h1>
-            <b-btn pill variant="success">Book</b-btn>
-          </b-carousel-slide>
+  <div class='container'>
+    <div class='carousel'>
+      <b-carousel
+        id='carousel-1'
+        v-model='slide'
+        :interval='4000'
+        controls
+        indicators
+        background='#ababab'
+        style='text-shadow: 1px 1px 2px #333;'
+        @sliding-start='onSlideStart'
+        @sliding-end='onSlideEnd'
+      >
+        <b-carousel-slide img-src='https://az820503.vo.msecnd.net/images/89630.jpg'> <!--Select dental img -->
+          <h2>Need to consult a specialist?</h2>
+          <b-btn pill variant='success' href='/'>Book</b-btn>
+        </b-carousel-slide>
 
-          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"><!--Select dental img -->
-            <h1>Find clinics near you!</h1>
-            <b-btn pill variant="success">Clinics</b-btn>
-          </b-carousel-slide>
+        <b-carousel-slide img-src='https://az820503.vo.msecnd.net/images/89630.jpg'><!--Select dental img -->
+          <h2>Find clinics near you!</h2>
+          <b-btn pill variant='success' href='/clinics'>Clinics</b-btn>
+        </b-carousel-slide>
 
-          <b-carousel-slide img-src="https://az820503.vo.msecnd.net/images/89630.jpg"><!--Select dental img -->
-            <h1>Who are we?</h1>
-            <b-btn pill variant="success">About us</b-btn>
-          </b-carousel-slide>
+        <b-carousel-slide img-src='https://az820503.vo.msecnd.net/images/89630.jpg'><!--Select dental img -->
+          <h2>Who are we?</h2>
+          <b-btn pill variant='success' href='/aboutus'>About us</b-btn>
+        </b-carousel-slide>
 
-        </b-carousel>
-      </div>
+      </b-carousel>
     </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8">
-          <h1>What do we offer?</h1>
-          <p><!--Rewrite Text with group -->
-            At Dentanoid, we provide a seamless online booking experience for multiple dental clinics across Gothenburg.
-            Our platform ensures a user-friendly interface,
-            making it easy for you to schedule appointments with various dental practitioners in the area.
-            we're all about real and meaningful communication.
-            Whether you're looking for routine check-ups, cosmetic dentistry, or specialized treatments, Dentanoid has you covered.
-            Our commitment is to offer a hassle-free booking process, allowing you to prioritize your oral health with convenience and efficiency.
-          </p>
-        </div>
-        <div class="col-2">
-          <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'"/>
-          <div>
-            <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'"/>
+  </div>
+  <div class='container'>
+    <div class='row'>
+      <div class='col-2'></div>
+      <div class='col-lg-8'>
+        <h1>What do we offer?</h1>
+        <p><!--Rewrite Text with group -->
+          At Dentanoid, we provide a seamless online booking experience for multiple dental clinics across Gothenburg.
+          Our platform ensures a user-friendly interface,
+          making it easy for you to schedule appointments with various dental practitioners in the area.
+          we're all about real and meaningful communication.
+          Whether you're looking for routine check-ups, cosmetic dentistry, or specialized treatments, Dentanoid has you
+          covered.
+          Our commitment is to offer a hassle-free booking process, allowing you to prioritize your oral health with
+          convenience and efficiency.
+        </p>
+        <p><!--Rewrite Text with group -->
+          At Dentanoid, we provide a seamless online booking experience for multiple dental clinics across Gothenburg.
+          Our platform ensures a user-friendly interface,
+          making it easy for you to schedule appointments with various dental practitioners in the area.
+          we're all about real and meaningful communication.
+          Whether you're looking for routine check-ups, cosmetic dentistry, or specialized treatments, Dentanoid has you
+          covered.
+          Our commitment is to offer a hassle-free booking process, allowing you to prioritize your oral health with
+          convenience and efficiency.
+        </p>
+      </div>
+      <div class='col-lg-2'>
+        <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'" /><!--Rewrite Text with group -->
+        <QuoteCard :quote="'Fobar bar fo lorem ipsum ipsum'" :name="'Lena'" :card-height="'auto'" :card-width="'auto'" /><!--Rewrite Text with group -->
+      </div>
+      <div class='col-lg-2'></div>
+      <div class='col-lg-8 text-center'>
+        <h1>Start your dental adventure today!</h1>
+        <div class='row'>
+          <div class='col-lg-4 col-sm-12 text-center lp-nav-btn'>
+            <b-btn size='lg' pill variant='success' href='/authentication'>Sign up</b-btn>
+          </div>
+          <div class='col-lg-4 col-sm-12 text-center lp-nav-btn'>
+            <b-btn size='lg' pill variant='success' href='/authentication'>Sign in</b-btn>
+          </div>
+          <div class='col-lg-4 col-sm-12 text-center lp-nav-btn'>
+            <b-btn size='lg' pill variant='success' href='/clinics'>Book</b-btn>
           </div>
         </div>
-        <div class="col-2"></div>
-        <div class="col-8">
-          <h1>Start your dental adventure today!</h1>
-          <div class="row">
-            <div class="col-4">
-              <b-btn size="lg" pill variant="success">Sign up</b-btn>
-            </div>
-            <div class="col-4">
-              <b-btn size="lg" pill variant="success">Sign in</b-btn>
-            </div>
-            <div class="col-4">
-              <b-btn size="lg" pill variant="success">Book </b-btn>
-            </div>
-          </div>
-        </div>
-        <div class="col-2"></div>
       </div>
+      <div class='col-2'></div>
     </div>
+  </div>
   </body>
 </template>
 
@@ -98,11 +108,11 @@ export default {
 
 <style scoped>
 .carousel {
-  margin-bottom: 10px;
+  margin-bottom: 2vh;
 }
 
-/** {*/
-/*  border-style: dashed;*/
-/*  border-color: green;*/
-/*}*/
+.lp-nav-btn {
+  margin-bottom: 1vh;
+}
+
 </style>
