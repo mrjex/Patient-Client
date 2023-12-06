d62f8ae58c3c484b9699c2b9932bd4e0e701a863
Bomanjo <gusbomjod@student.gu.se>
2023-12-06 04:49:37 +0100

    #10 (ongoing) add view to showcase timeslots

---
+++ client/src/views/FreeTimeslots.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex-container">
+        <div class="m-2"><h1>Available times</h1></div>
+        <timeslotAccordion>
+
+        </timeslotAccordion>
+    </div>
+</template>
+
+<script>
+import timeslotAccordion from '../component/timeslots/timeslotAccordion.vue'
+export default {
+  name: 'FreeTimeslotsView',
+  components: {
+    timeslotAccordion
+  }
+}
+</script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+}
+</style>
