71ae1e2cc1d24493cdf4fc8bbcfd83155e753d0e
Bomanjo <gusbomjod@student.gu.se>
2023-12-13 00:14:43 +0100

    #15 (ongoing) add initial structure for clinics page

---
+++ client/src/component/clinics/ClinicList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-fluid text-center">
+    <b-card no-body :v-for="clinic in clinics" :key="clinic._id">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle="'clinic' + clinic._id" variant="info">{{ clinic.clinicName }}</b-button>
+      </b-card-header>
+      <b-collapse :id="'clinic' + clinic._id" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <div v-if="clinic.dentists.length === 0">This clinic has no employees</div>
+          <div v-else>
+            <dentist v-for="dentist in clinic.dentists"
+            :key="dentist._id"
+            :dentist="dentist"
+            @showTimeslots="handleShowTimeSlots"/>
+          </div>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <!--TimeslotComponent-->
+    <timeslotAccordion>
+
+    </timeslotAccordion>
+  </div>
+</template>
+
+<script>
+import timeslotAccordion from './timeSlotAccordion.vue'
+export default {
+  data() {
+    return {
+      clinics: []
+    }
+  },
+  components: {
+    timeslotAccordion
+  },
+  methods: {
+    getClinics() {
+      // get clinicss
+    },
+    handleShowTimeSlots(dentistId) {
+      // Hide accordion and display timeslots for dentist._id received
+    }
+  },
+  mounted() {
+    this.getClinics()
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ client/src/views/Clinics.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex-container">
+    <div class="m-2">
+      <h1>Clinics</h1>
+    </div>
+    <div class="container-fluid">
+      <clinicList></clinicList>
+    </div>
+  </div>
+</template>
+<!--This view is hardcoded for testing purposes and is not meant to be its own view in production-->
+<script>
+import clinicList from '../component/clinics/ClinicList.vue'
+export default {
+  name: 'ClinicsView',
+  components: {
+    clinicList
+  }
+}
+</script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex-direction: column;
+}
+</style>
