2d6be0360f45c418a0d4acd6b3e5b6f157f5a7c3
Mohamad Khalil <mohamadk@student.chalmers.se>
2024-01-06 18:14:12 +0100

    #11 (Ongoing) Integrate necessary API libraries

--- client/public/maps/map-utils.js
+++ client/public/maps/map-utils.js
@@ -45,6 +45,11 @@ function confirmExecutionConditions(mapMode) {
   return (isAllowedPage === true && currentMapMode === mapMode)
 }
 
+function integrateAPIKey() {
+  (g => { let h; let a; let k; const p = 'The Google Maps JavaScript API'; const c = 'google'; const l = 'importLibrary'; const q = '__ib__'; const m = document; let b = window; b = b[c] || (b[c] = {}); const d = b.maps || (b.maps = {}); const r = new Set(); const e = new URLSearchParams(); const u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement('script')); e.set('libraries', [...r, 'places'] + ''); for (k in g)e.set(k.replace(/[A-Z]/g, t => '_' + t[0].toLowerCase()), g[k]); e.set('callback', c + '.maps.' + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + ' could not load.')); a.nonce = m.querySelector('script[nonce]')?.nonce || ''; m.head.append(a) })); d[l] ? console.warn(p + ' only loads once. Ignoring:', g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })
+  ({ key: 'AIzaSyBezKgTO8Fu1ymaIoAoToNn0g5ZMjgSR4Y', v: 'weekly' })
+}
+
 function changeMapMode(newMapMode) {
   currentMapMode = newMapMode
 }
@@ -165,6 +170,8 @@ function updateMapUI() {
     updateRadius()
   }
 }
+
+integrateAPIKey()
 
 export {
   confirmExecutionConditions, changeMapMode, currentMapMode, changeRadius, currentRadius, getZoomLevel,
