d82035bb7f5c76a6f6a78a578c40277af7ca0442
Bomanjo <gusbomjod@student.gu.se>
2024-01-08 03:54:44 +0100

    #18 (ongoing) add booking functionality to map page and filter time slot functionality

--- client/src/views/MapPage.vue
+++ client/src/views/MapPage.vue
@@ -1,11 +1,17 @@
 <template>
   <body>
-      <div>
-        <MapComponent/>
+    <div>
+      <MapComponent />
+    </div>
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="flex-container">
+        <timeSpanModal class="mb-3" @selectedTime="handleFilterTimes" />
+        <ClinicList :clinics="clinics" v-if="showClinicList" @clinicClick="handleClinicClick" />
+        <timeslotAccordion :availableTimes="filteredAvailableTimes" v-if="showTimeslots"
+          @showClinics="handleDisplayClinics" />
       </div>
-      <div>
-        <ClinicList :clinics="clinics" />
-      </div>
+    </div>
+
   </body>
 </template>
 
@@ -13,6 +19,9 @@
 import MapComponent from '../components/MapComponent.vue'
 import ClinicList from '../component/clinics/ClinicList.vue'
 import { clinicsData } from '../../public/maps/map-utils.js'
+import timeSpanModal from '../component/timeslots/timeSpanModal.vue'
+import { getTimeWindowTimeSlots } from '@/utility/timeslotUtils'
+import timeslotAccordion from '../component/timeslots/timeslotAccordion.vue'
 
 export default {
   name: 'MapPage',
@@ -20,7 +29,16 @@ export default {
     return {
       clinics: [],
       updateInterval: 100,
-      previousClinicRequest: ''
+      previousClinicRequest: '',
+      availableTimes: [],
+      showClinicList: true,
+      showTimeslots: false,
+      selectedClinicId: null
+    }
+  },
+  computed: {
+    filteredAvailableTimes() {
+      return this.availableTimes.filter((availableTime) => availableTime.clinic_id === this.selectedClinicId)
     }
   },
   methods: {
@@ -41,6 +59,32 @@ export default {
         },
         this.updateInterval
       )
+    },
+    async handleFilterTimes(filter) {
+      try {
+        const clinicIds = []
+        this.clinics.forEach((clinic) => {
+          clinicIds.push(clinic._id.$oid)
+        })
+        console.log(clinicIds)
+        const res = await getTimeWindowTimeSlots(clinicIds, filter.timespan)
+        if (res.data.availabletimes) {
+          this.availableTimes = res.data.availabletimes
+          const availableClinicIds = new Set(this.availableTimes.map(at => at.clinic_id))
+          this.clinics = this.clinics.filter(clinic => availableClinicIds.has(clinic._id.$oid))
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    handleDisplayClinics() {
+      this.showClinicList = true
+      this.showTimeslots = false
+    },
+    handleClinicClick(clinicId) {
+      this.showClinicList = false
+      this.showTimeslots = true
+      this.selectedClinicId = clinicId
     }
   },
   created() {
@@ -48,7 +92,20 @@ export default {
   },
   components: {
     MapComponent,
-    ClinicList
+    ClinicList,
+    timeSpanModal,
+    timeslotAccordion
   }
 }
 </script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  max-height: fit-content;
+  min-width: 40vw;
+}
+</style>
