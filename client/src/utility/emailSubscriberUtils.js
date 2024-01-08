0efe9d32c8d746b198ece9e5d38305fcca7ee4a2
JamesKlouda <gusklouja@student.gu.se>
2024-01-08 17:31:09 +0100

    #14 (ongoing) Add crud utilitys for email subscription

--- client/src/utility/emailSubscriberUtils.js
+++ client/src/utility/emailSubscriberUtils.js
@@ -4,11 +4,46 @@ export async function postSubscriber(subscriber) {
   try {
     const res = await Api.post('subscriber/', subscriber)
     if (res.data.status === 201) {
-      console.log('Sub created')
-      return true
+      return res.data
     }
     return false
   } catch (err) {
     console.error('Error when posting subscriber', err)
+  }
+}
+
+export async function getSubscription() {
+  try {
+    const res = await Api.get('subscriber/')
+    if (res.data.status === 200 || res.data.status === 304) {
+      return res.data.subscriber
+    }
+    return false
+  } catch (err) {
+    console.error('Error when getting subscriber', err)
+  }
+}
+
+export async function updateSubscription(subscriber) {
+  try {
+    const res = await Api.patch('subscriber/', subscriber)
+    if (res.data.status === 200 || res.data.status === 304) {
+      return res
+    }
+    return false
+  } catch (err) {
+    console.error('Error when getting subscriber', err)
+  }
+}
+
+export async function deleteSubscription() {
+  try {
+    const res = await Api.delete('subscriber/')
+    if (res.data.status === 200 || res.data.status === 304) {
+      return res
+    }
+    return false
+  } catch (err) {
+    console.error('Error when getting subscriber', err)
   }
 }
